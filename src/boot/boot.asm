45473f5a1f0d20a25d491081ae53fd2b3b76e194
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2024-02-03 11:04:12 +0530

    Enabling A20 line

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -73,6 +73,11 @@ load32:
     mov ss,ax
     mov ebp,0x00200000 ; avoids conflicting with system-specific memory regions, that's why this memory location is chosen for protected mode 
     mov esp, ebp  
+
+    ;Enabling A20 line
+    in al,0x92
+    or al,2
+    out 0x92,al
     jmp $
 
 times 510-($-$$) db 0
